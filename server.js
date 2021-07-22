2b50d49cae8bced7f26500415b95b409efd6a752
Rüdiger Bachmann <ruediger.bachmann@nexontis.com>
2021-07-22 08:46:46 +0200

    Remove one agent

--- server.js
+++ server.js
@@ -1,13 +1,5 @@
-try {
-    require('@dynatrace/oneagent')();
-  } catch (e) {
-    console.warn(`OneAgent: ${ e.message }`);
-  }
-
 var express = require('express');
 var app = express();
-
-const Sdk = require('@dynatrace/oneagent-sdk');
 
 let port = (process.env.PORT || '9000');
  
@@ -27,11 +19,6 @@ app.get('/user/:id', function (req, res, next) {
     });
 
 app.get('/user/:id', function (req, res, next) {
-    getApi().traceIncomingRemoteCall({
-        serviceName: 'sap-papm-test-dynatrace',
-        serviceMethod: 'after request handler',
-        serviceEndpoint: `${req.hostname} `
-    });
     console.log('after request handler');
     next();
 });
@@ -40,12 +27,5 @@ app.listen(port, function () {
     console.log('Example app listening on port 3000!');
 });
 
-function getApi() {
-    if (!this._Api){ 
-        this._Api = Sdk.createInstance();
-        console.log('Got Api')
-    }
-    return this._Api;
-}
 
 
