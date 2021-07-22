94d6327edc7a6286a1f2b7b846329f217b9c08ad
Rüdiger Bachmann <ruediger.bachmann@nexontis.com>
2021-07-22 08:42:51 +0200

    Add dynatrace with one agent

---
+++ callService.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+while [ true ]; do
+    # body
+    curl --location --request GET 'https://sap-papm-test-dynatrace.cfapps.eu10.hana.ondemand.com/user/jklsdslf'
+    sleep 1s
+done
+
+
--- server.js
+++ server.js
@@ -1,5 +1,13 @@
+try {
+    require('@dynatrace/oneagent')();
+  } catch (e) {
+    console.warn(`OneAgent: ${ e.message }`);
+  }
+
 var express = require('express');
 var app = express();
+
+const Sdk = require('@dynatrace/oneagent-sdk');
 
 let port = (process.env.PORT || '9000');
  
@@ -19,10 +27,25 @@ app.get('/user/:id', function (req, res, next) {
     });
 
 app.get('/user/:id', function (req, res, next) {
+    getApi().traceIncomingRemoteCall({
+        serviceName: 'sap-papm-test-dynatrace',
+        serviceMethod: 'after request handler',
+        serviceEndpoint: `${req.hostname} `
+    });
     console.log('after request handler');
     next();
 });
 
 app.listen(port, function () {
-    console.log('Example app listening on port 3000!')
+    console.log('Example app listening on port 3000!');
 });
+
+function getApi() {
+    if (!this._Api){ 
+        this._Api = Sdk.createInstance();
+        console.log('Got Api')
+    }
+    return this._Api;
+}
+
+
